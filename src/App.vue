5cf985f5650272206dd82e34b4872a6facbec58d
Akram997 <l303216115@qq.com>
2019-07-11 10:29:44 +0800

    init my onevue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <van-nav-bar class="title" title="千峰-Vue项目" fixed> </van-nav-bar>
+    <van-tabbar v-model="active">
+      <van-tabbar-item icon="home-o">首页</van-tabbar-item>
+      <van-tabbar-item icon="comment-o">消息</van-tabbar-item>
+      <van-tabbar-item icon="friends-o" info="5">通讯录</van-tabbar-item>
+      <van-tabbar-item icon="setting-o" info="20">设置</van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Vue from "vue";
+import { NavBar,Tabbar, TabbarItem } from "vant";
+Vue.use(NavBar).use(Tabbar).use(TabbarItem);;
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      active:0
+    }
+  },
+  methods: {
+    
   }
-}
+};
 </script>
 
-<style>
+<style scoped>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding-top: 46px;
+}
+.title{
+  background-color: turquoise;
+  border-bottom: 1px solid #ccc;
+}
+.van-tabbar{
+  border-top: 1px solid #ccc;
 }
 </style>
+
